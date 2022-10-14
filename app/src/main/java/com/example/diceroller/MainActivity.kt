eea3355f351f2f6af72c0e71e7612272862ff97f
Efrain Arturo Gomez Fajardo <efrainarturog@gmail.com>
2022-10-13 20:52:55 -0600

    Added second dice and special texts

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -14,24 +15,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Create the two dices to be used
+        val dice = Dice(6)
+        val dice2 = Dice(6)
+
         // Add a listener event to the "Roll" button
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDice(dice, dice2) }
 
         // Roll dice when app boots up
-        rollDice()
+        rollDice(dice, dice2)
     }
 
-    private fun rollDice() {
-        // Create a new 6-sized dice and roll it
-        val dice = Dice(6)
-        val diceRoll: Int = dice.roll()
+    private fun rollDice(dice: Dice, dice2: Dice) {
+
+        val diceRoll1: Int = dice.roll()
+        val diceRoll2: Int = dice2.roll()
+        val total = diceRoll1 + diceRoll2
+
+        // Modify the total based on the roll
+        val textView: TextView = findViewById(R.id.textView2)
+        textView.text = total.toString()
+
+        // Adding some special text depending on the roll
+        val specialText: TextView = findViewById(R.id.textView3)
+        if (diceRoll1 == diceRoll2) {
+            specialText.text = "Doubles!"
+        } else {
+            specialText.text = "Just a pair of dice"
+        }
 
         // Find the imageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
 
         // Updating the image based on the dice roll
-        when (diceRoll) {
+        when (diceRoll1) {
             1 -> diceImage.setImageResource(R.drawable.dice_1)
             2 -> diceImage.setImageResource(R.drawable.dice_2)
             3 -> diceImage.setImageResource(R.drawable.dice_3)
@@ -39,8 +58,18 @@ class MainActivity : AppCompatActivity() {
             5 -> diceImage.setImageResource(R.drawable.dice_5)
             6 -> diceImage.setImageResource(R.drawable.dice_6)
         }
+
+        when (diceRoll2) {
+            1 -> diceImage2.setImageResource(R.drawable.dice_1)
+            2 -> diceImage2.setImageResource(R.drawable.dice_2)
+            3 -> diceImage2.setImageResource(R.drawable.dice_3)
+            4 -> diceImage2.setImageResource(R.drawable.dice_4)
+            5 -> diceImage2.setImageResource(R.drawable.dice_5)
+            6 -> diceImage2.setImageResource(R.drawable.dice_6)
+        }
         // Add a description to be read, if needed
-        diceImage.contentDescription = dice.roll().toString()
+        diceImage.contentDescription = diceRoll1.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 }
 
